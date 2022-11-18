6fb4d7380f10152b4e237127de5afe1e23f1517b
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-18 16:11:12 +0100

    initialized design

--- js/main.js
+++ js/main.js
@@ -1,58 +1,64 @@
 // Visualizzare in pagina 5 numeri casuali, dopo un timer di 30 secondi i numeri scompaiono e l'utene deve inserirli uno alla volta, ìl software dirà quanti numeri erano giusti e quanti erano sbagliati
 
 // Manipolazione DOM
-const output = document.getElementById("numeri");
 const input = document.getElementById("input");
-const corretti = document.getElementById("corretti")
+const output = document.getElementById("numeri");
+const risultato = document.getElementById("risultato")
 const button = document.getElementById("button");
 
 // Array
 let arrNum = [];
-let numeriGiusti = 0;
-let user = [];
+let score = 0;
+let numeriUtente = [];
 
 // Generiamo 5 numeri random
-arrNum = [1,1,1,1,1];
+arrNum = numRanUnici(5);
 
 // Stampare i numeri a schermo a schermo
 for (let i = 0; i < arrNum.length; i++){
-    output.innerHTML += `<li>${arrNum[i]}</li>`    
+    output.innerHTML += `<li>${arrNum[i]}</li>`;  
 }
 
 // Creare un timer di 30 secondi che nasconde i numeri
-const time = setTimeout(
-    function(){
-        output.innerHTML = ""    
-    }
-,3000);
+// const time = setTimeout(
+//     function(){
 
-// Far inserire i numeri all'utente
+//         // Rimuovere completamente la lista per evitare di vedere i risultati nel codice sorgente
+//         output.innerHTML = "";  
+//     }
+// ,3000); // DEBUG TIME
+
+// Bottone Inserire numero
 button.addEventListener("click",
     function(){
+
+        // Condizione per evitare una stringa come dato
         if (input.value >= 0){
-            user.push(parseInt(input.value));
+            numeriUtente.push(parseInt(input.value));
             input.value = null;
-            if (user.length >= arrNum.length){
+
+            // Condizione per fermare l'input quando si arriva al numero massimo
+            if (numeriUtente.length >= arrNum.length){
                 input.style.display = "none";
                 button.style.display = "none";
 
+                // Controllo numeri giusti
                 for (let i = 0; i < arrNum.length; i++){
-                    if (arrNum[i] === user[i]){
-                        numeriGiusti++;
+                    if (arrNum[i] === numeriUtente[i]){
+                        score++;
                     }
                 }
 
 
                 // Stampare quanti numeri sono giusti
+                if (score === arrNum.length){
+                    risultato.innerHTML = "I numeri giusti sono: " + score + ", hai indovinato tutti i numeri!"
 
-                if (numeriGiusti === arrNum.length){
-                    corretti.innerHTML = "I numeri giusti sono: " + numeriGiusti + ", hai indovinato tutti i numeri!"
-
-                } else if (numeriGiusti === 0){
-                    corretti.innerHTML = "Hai sbagliato " + (arrNum.length - numeriGiusti) + " numeri, non ne hai indovinato nessuno!"
+                } else if (score === 0){
+                    risultato.innerHTML = "Hai sbagliato " + (arrNum.length - score) + " numeri, non ne hai indovinato nessuno!"
 
                 } else{
-                    corretti.innerHTML = "I numeri giusti sono: " + numeriGiusti + " e quelli sbagliati " + (arrNum.length - numeriGiusti);
+                    risultato.innerHTML = "I numeri giusti sono: " + score + " e quelli sbagliati " + (arrNum.length - score);
                 }
                 
             }
