024e8dbd585644b9a5a4bf09a2ab3e70a5576010
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-18 15:18:36 +0100

    First Debug Version Done

--- js/main.js
+++ js/main.js
@@ -2,30 +2,59 @@
 
 // Manipolazione DOM
 const output = document.getElementById("numeri");
+const input = document.getElementById("input");
+const corretti = document.getElementById("corretti")
 
 // Array
 let arrNum = [];
+let numeriGiusti = 0;
 
 // Generiamo 5 numeri random
-for (let i = 0; i < 5; i++){
-    arrNum.push(numRandom(0, 100))
-}
+arrNum = numRanUnici(5);
+console.log(arrNum)
 
 // Stampare i numeri a schermo a schermo
 for (let i = 0; i < arrNum.length; i++){
-    output.innerHTML += `<li>${arrNum[i]}</li>`
-    
+    output.innerHTML += `<li>${arrNum[i]}</li>`    
 }
 
 // Creare un timer di 30 secondi che nasconde i numeri
+const time = setTimeout(
+    function(){
+        output.innerHTML = ""    
+    }
+,3000);
 
 // Chiedere all'utente di inserire i numeri
+for (let i = 0; i < arrNum.length; i++){
+    let user = parseInt(prompt("inseri"));
+    if (arrNum.includes(user)){
+        numeriGiusti++
+    }
+}
 
 // Verificare quanti numeri sono giusti
+corretti.innerHTML = "I numeri giusti sono: " + numeriGiusti + " e quelli sbagliati " + (arrNum.length - numeriGiusti);
 
-// Funzioni
+
+
+// Funzioni-----------------------------
 
 // Funzione che genera numeri random
 function numRandom(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Funzione per generare numeri random unici
+function numRanUnici(quantità){
+    let array = [];
+    let number
+    while (array.length < quantità){
+        number = numRandom(0, 100);
+        if (!array.includes(number)){
+            array.push(number);
+        }
+    }
+
+    return array
 }
